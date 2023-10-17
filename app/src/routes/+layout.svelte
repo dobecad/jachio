d57ea933c9f683547943dbbc83858851da56c35d
jachii <jettymcfiggs@gmail.com>
2023-10-16 23:04:51 -0400

    Added basic routes

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -31,44 +31,48 @@
 	</div>
 </Drawer>
 
-<AppShell slotSidebarLeft="bg-surface-500/5 w-0">
-	<svelte:fragment slot="header">
-		<AppBar
-			background="bg-inherit"
-			gridColumns="grid-cols-3"
-			slotDefault="place-self-center"
-			slotTrail="place-content-end"
-		>
-			<svelte:fragment slot="lead">
-				<div class="flex items-center">
-					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-						<span>
-							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-								<rect width="100" height="20" />
-								<rect y="30" width="100" height="20" />
-								<rect y="60" width="100" height="20" />
-							</svg>
-						</span>
-					</button>
-					<h2
-						class="h2 uppercase bg-clip-text text-transparent bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500"
-					>
-						Jachio
-					</h2>
-				</div>
-			</svelte:fragment>
-			<Navigation vertical={false} />
-			<svelte:fragment slot="trail">
-				<div class="hidden lg:flex space-x-4">
-					<Media />
-				</div>
-				<LightSwitch />
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
+<div class="container mx-auto">
+	<AppShell slotSidebarLeft="bg-surface-500/5 w-0">
+		<svelte:fragment slot="header">
+			<AppBar
+				background="bg-inherit"
+				gridColumns="grid-cols-3"
+				slotDefault="place-self-center"
+				slotTrail="place-content-end"
+			>
+				<svelte:fragment slot="lead">
+					<div class="flex items-center">
+						<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+							<span>
+								<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+									<rect width="100" height="20" />
+									<rect y="30" width="100" height="20" />
+									<rect y="60" width="100" height="20" />
+								</svg>
+							</span>
+						</button>
+						<div class="flex items-center">
+							<img src="/BlackHole2.svg" alt="blackhole" class="w-full h-24" />
+							<h2
+								class="h2 uppercase bg-clip-text text-transparent bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 hidden lg:flex"
+							>
+								Jachii
+							</h2>
+						</div>
+					</div>
+				</svelte:fragment>
+				<Navigation vertical={false} />
+				<svelte:fragment slot="trail">
+					<div class="hidden lg:flex space-x-4">
+						<Media />
+					</div>
+					<LightSwitch />
+				</svelte:fragment>
+			</AppBar>
+		</svelte:fragment>
 
-	<svelte:fragment slot="sidebarLeft">
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+		<svelte:fragment slot="sidebarLeft" />
+		<!-- Page Route Content -->
+		<slot />
+	</AppShell>
+</div>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,16 +1,2 @@
-<!-- <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome to Skeleton.</h2>
-	</div>
-</div> -->
-
-<div class="container mx-auto p-10 space-y-4">
-	<div class="card text-center">
-		<header class="card-header text-2xl">I'm Jace</header>
-		<section class="p-4">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate nostrum, vero explicabo
-			sunt ipsum asperiores. Molestias nostrum mollitia cumque veniam eum error atque, dolores
-			nesciunt voluptas ea consectetur sed delectus.
-		</section>
-	</div>
+<div class="container mx-auto space-y-4">
 </div>
