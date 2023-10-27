5b60266d8df203ae70126ea13749702af2fa1411
jachii <jettymcfiggs@gmail.com>
2023-10-27 01:35:04 -0400

    Typo

--- app/src/lib/components/Intro.svelte
+++ app/src/lib/components/Intro.svelte
@@ -6,5 +6,5 @@
     <div class="text-6xl">
         <p>Hi, I'm</p>
     </div>
-    <div class="pt-8 text-8xl"><span class="text-red-600">J</span>ace</div>
+    <div class="pt-8 text-8xl"><span class="text-red-600">J</span>ACE</div>
 </div>
