a7e577acd3fb67db630d814c8b413e0745d00dc7
jachii <jettymcfiggs@gmail.com>
2023-10-27 01:34:59 -0400

    History section

---
+++ app/src/lib/components/History.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import HistoryEntry from './HistoryEntry.svelte';
+	import Sectionname from './Sectionname.svelte';
+
+	const history: { name: string; dates: string; title: string; bulletpoints: string[] }[] = [
+		{
+			name: 'U.S. Government',
+			dates: '2018 - 2023',
+			title: 'Software and DevOps Engineer',
+			bulletpoints: ['Hello World!', 'Its a me']
+		}
+	];
+</script>
+
+<div class="flex justify-center text-8xl">
+	<Sectionname text={'History'} />
+</div>
+
+<div class="w-full">
+	{#each history as historyEntry}
+		<HistoryEntry
+			name={historyEntry.name}
+			dates={historyEntry.dates}
+			title={historyEntry.title}
+			bulletpoints={historyEntry.bulletpoints}
+		/>
+	{/each}
+</div>
---
+++ app/src/lib/components/HistoryEntry.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let name: string;
+    export let dates: string;
+    export let title: string;
+    export let bulletpoints: string[]; 
+</script>
+
+<div class="flex justify-between pb-2">
+    <div class="text-6xl">{name}</div>
+    <div class="text-3xl flex items-end opacity-50">{dates}</div>
+</div>
+<hr class="opacity-50" />
+<div class="flex justify-between pt-4">
+    <div class="text-2xl">{title}</div>
+    <ul class="list-disc w-1/3 pb-2">
+        {#each bulletpoints as bulletpoint}
+            <li class="text-xl">{bulletpoint}</li>
+        {/each}
+    </ul>
+</div>
