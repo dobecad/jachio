f346f9761b68dbdf65a734fb93e040f646416b71
jachii <jettymcfiggs@gmail.com>
2023-10-27 02:01:35 -0400

    Experience

---
+++ app/src/lib/components/Experience.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import ExperienceEntry from './ExperienceEntry.svelte';
+	import Sectionname from './Sectionname.svelte';
+
+	const experience: { img: string; name: string }[] = [
+		{ img: 'BlackHole2.png', name: 'Rust' },
+		{ img: 'BlackHole2.png', name: 'Go' },
+		{ img: 'BlackHole2.png', name: 'Python' },
+		{ img: 'BlackHole2.png', name: 'TypeScript' },
+		{ img: 'BlackHole2.png', name: 'JavaScript' },
+		{ img: 'BlackHole2.png', name: 'Java' },
+		{ img: 'BlackHole2.png', name: 'C' },
+		{ img: 'BlackHole2.png', name: 'C++' },
+		{ img: 'BlackHole2.png', name: 'Kubernetes' },
+		{ img: 'BlackHole2.png', name: 'Docker' },
+		{ img: 'BlackHole2.png', name: 'ArgoCD' },
+		{ img: 'BlackHole2.png', name: 'CI/CD' }
+	];
+</script>
+
+<div class="flex justify-center text-8xl pb-4">
+	<Sectionname text="Experience" />
+</div>
+
+<div class="w-full pt-4 grid grid-cols-6 gap-8 justify-items-center">
+	{#each experience as e}
+		<ExperienceEntry img={e.img} name={e.name} />
+	{/each}
+</div>
---
+++ app/src/lib/components/ExperienceEntry.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let img: string;
+    export let name: string;
+</script>
+
+<div class="w-32 h-fit border-slate-200 rounded-xl border-4">
+    <div>
+        <img src="{img}" alt="{name}" />
+    </div>
+    <div class="text-xl text-center">
+       {name} 
+    </div>
+</div>
