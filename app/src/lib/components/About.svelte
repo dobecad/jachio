e76e3482a3a0ddcc98a90a9bc2acaccd79e5e032
jachii <jettymcfiggs@gmail.com>
2023-10-31 12:11:04 -0400

    Improve resizing

--- app/src/lib/components/About.svelte
+++ app/src/lib/components/About.svelte
@@ -7,11 +7,15 @@
     <Sectionname text={"About"} />
 </div>
 
-<div class="flex justify-center">
-    <img src="BlackHole2.png" alt="logo" class="w-80 h-80">
-    <div class="grid grid-rows-3 pl-80 text-xl">
-        <div class="col-span-2">My heart beats for simplicity, and I channel that passion into crafting software that's here to make your life a whole lot easier.</div>
-        <div class="col-span-2">I have strong interests in distributed systems, full-stack development, API development, container orchestration, and building performant, cost-effective cloud-native applications.</div>
-        <div class="col-span-2">When I am not trying to make your life easier, I am often either learning some new, bleeding edge technology, trying to get good at a new video game, contributing to open source projects, noodling on the guitar, or catching up on my backlog of engineering books.</div>
+<div class="lg:grid lg:grid-cols-2">
+    <div class="flex justify-center">
+        <img src="BlackHole2.png" alt="logo" class="w-80 h-80">
+    </div>
+    <div class="text-2xl pt-8 lg:pt-0">
+        <p class="flex">My heart beats for simplicity, and I channel that passion into crafting software that's here to make your life a whole lot easier.</p>
+        <br>
+        <p class="flex">I have strong interests in distributed systems, full-stack development, API development, container orchestration, and building performant, cost-effective cloud-native applications.</p>
+        <br>
+        <p class="flex">When I am not trying to make your life easier, I am often either learning some new, bleeding edge technology, trying to get good at a new video game, contributing to open source projects, noodling on the guitar, or catching up on my backlog of engineering books.</p>
     </div>
 </div>
