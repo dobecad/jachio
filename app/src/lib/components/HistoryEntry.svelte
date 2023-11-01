97f5fbeb01c46cdb5e22ae69d349282b17d5c4e0
jachii <jettymcfiggs@gmail.com>
2023-10-31 20:48:04 -0400

    Formatting at smaller screens

--- app/src/lib/components/HistoryEntry.svelte
+++ app/src/lib/components/HistoryEntry.svelte
@@ -10,11 +10,11 @@
     <div class="text-2xl flex items-end opacity-50">{dates}</div>
 </div>
 <hr class="opacity-50" />
-<div class="flex justify-between pt-4">
-    <div class="text-2xl">{title}</div>
-    <ul class="list-disc w-1/2 pb-2">
+<div class="lg:flex lg:justify-between pt-4">
+    <div class="text-2xl lg:flex text-center">{title}</div>
+    <ul class="list-disc lg:w-1/2 w-full pb-2 text-center lg:text-left pt-4">
         {#each bulletpoints as bulletpoint}
-            <li class="text-xl">{bulletpoint}</li>
+            <li class="text-xl pb-4">{bulletpoint}</li>
         {/each}
     </ul>
 </div>
