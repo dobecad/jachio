d461545d76159ae0e9581f60e30476cd27a8372b
jachii <jettymcfiggs@gmail.com>
2023-11-02 00:00:57 -0400

    Added green outline

--- app/src/lib/components/ContactMe.svelte
+++ app/src/lib/components/ContactMe.svelte
@@ -1,5 +1,5 @@
 <button
-	class="rounded-lg ring-slate-200 ring-offset-2 ring-2 p-3 text-red-800 text-xl transition hover:duration-300 hover:-translate-y-1 hover:scale-110 ease-in-out"
+	class="rounded-lg ring-lime-500 ring-4 p-3 text-red-800 text-xl bg-slate-900 transition hover:duration-300 hover:-translate-y-1 hover:scale-110 ease-in-out"
 	style="color: #9C1F58"
 >
 	Contact Me
