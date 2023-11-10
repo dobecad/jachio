f95f55fe5f8c0ffad4666ae0cead89f6decd6e0c
jachii <jettymcfiggs@gmail.com>
2023-11-10 01:43:43 -0500

    Added project interactive animation

--- app/src/lib/components/Project.svelte
+++ app/src/lib/components/Project.svelte
@@ -1,16 +1,91 @@
 <script lang="ts">
-    export let img: string;
-    export let name: string;
-    export let description: string;
+	import { onMount } from 'svelte';
+	import gsap from 'gsap';
+	import Background from './Background.svelte';
+
+	export let img: string;
+	export let name: string;
+	export let id: number;
+	export let description: string;
+	export let tags: string[];
+	let toggled = false;
+
+	const tileSize = 25;
+	let idName = 'project-' + name.toLowerCase().replaceAll(' ', '') + id.toString();
+	let columns = 0;
+	let rows = 0;
+
+	function handleOnClick(indx: number) {
+		toggled = toggled ? false : true;
+		gsap.to(`.${idName}-tiles`, {
+			backgroundColor: '#9C1F58',
+			opacity: 1,
+			stagger: {
+				amount: 0.8,
+				grid: [rows, columns],
+				from: indx
+			}
+		});
+		gsap.to(`.${idName}-tiles`, {
+			opacity: 0,
+			delay: 0.2,
+			stagger: {
+				amount: 0.8,
+				grid: [rows, columns],
+				from: indx
+			}
+		});
+	}
+
+	function createTile(indx: number): HTMLDivElement {
+		const tile = document.createElement('div');
+		tile.classList.add(`${idName}-tiles`);
+		tile.classList.add('cursor-pointer');
+		tile.classList.add('z-10');
+		tile.onclick = (e) => handleOnClick(indx);
+		return tile;
+	}
+
+	function createTiles(container: HTMLElement, quantity: number) {
+		Array.from(Array(quantity)).map((tile, indx) => {
+			container.appendChild(createTile(indx));
+		});
+	}
+
+	function createGrid() {
+		const container = document.getElementById(idName);
+		if (!container) {
+			return;
+		}
+		columns = Math.floor(container.offsetWidth / tileSize);
+		rows = Math.floor(container.offsetHeight / tileSize);
+		container.style.setProperty('--columns', columns.toString());
+		container.style.setProperty('--rows', rows.toString());
+		createTiles(container, columns * rows);
+	}
+
+	onMount(() => {
+		createGrid();
+	});
 </script>
 
-<div class="w-80 h-fit border-slate-200 border-4 rounded-lg">
-	<div>
-        <img src={img} alt="Project" />
-    </div>
-    <hr />
-	<div class="text-xl text-center">{name}</div>
-	<div>
-        {description}
-	</div>
+<div id={idName} class="w-80 h-80 border-slate-200 border-4 rounded-lg bg-slate-900 tiles relative">
+	{#if toggled}
+		<div class="w-full h-full absolute z-0 project-card-info overflow-x-auto overflow-y-auto">
+			<div class="text-xl text-center pt-2">{description}</div>
+			<div
+				class="flex flex-wrap w-full justify-center pt-4 gap-4"
+			>
+				{#each tags as tag}
+					<div class="flex bg-violet-600 opacity-80 pl-2 pr-2 w-fit rounded-full font-bold text-center">
+						{tag}
+					</div>
+				{/each}
+			</div>
+		</div>
+	{:else}
+		<div class="flex w-full h-full justify-center items-center absolute z-0 project-card-info">
+			<div class="text-xl text-center">{name}</div>
+		</div>
+	{/if}
 </div>
--- app/src/lib/components/Projects.svelte
+++ app/src/lib/components/Projects.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
 	import Project from './Project.svelte';
 	import Sectionname from './StyledWord.svelte';
-	const projects: { img: string; name: string; description: string }[] = [
+	const projects: { img: string; name: string; description: string; tags: string[] }[] = [
 		{
 			img: 'BlackHole2.png',
 			name: 'Vod Recap',
-			description: 'lorem ipsum'
+			description: 'lorem ipsum',
+			tags: ['Python', 'Django', 'FastAPI', 'PostgreSQL', 'JavaScript']
 		},
-		{ img: 'BlackHole2.png', name: 'MAL-API', description: 'lorem ipsum' },
-		{ img: 'BlackHole2.png', name: 'Orbitix', description: 'lorem ipsum' }
+		{ img: 'BlackHole2.png', name: 'MAL-API', description: 'lorem ipsum', tags: ['Rust', 'Serde'] },
+		{ img: 'BlackHole2.png', name: 'Orbitix', description: 'lorem ipsum', tags: ['Rust', 'gRPC'] },
+		{
+			img: 'BlackHole2.png',
+			name: 'Quipose',
+			description: 'lorem ipsum',
+			tags: ['Rust', 'Leptos', 'SurrealDB', 'Svelte']
+		},
+		{
+			img: 'BlackHole2.png',
+			name: 'Compose Validatr',
+			description: 'lorem ipsum',
+			tags: ['Rust', 'Serde']
+		},
+		{
+			img: 'BlackHole2.png',
+			name: 'Portfolio',
+			description: 'lorem ipsum',
+			tags: ['Svelte', 'TailwindCSS', 'GSAP']
+		}
 	];
 </script>
 
@@ -16,8 +35,56 @@
 	<a href="#Projects"><Sectionname text={'Projects'} /></a>
 </div>
 
-<div id="projects" class="flex justify-center pt-40 w-full">
-	{#each projects as project}
-		<Project img={project.img} name={project.name} description={project.description} />
-	{/each}
+<div class="pt-40">
+	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full gap-16">
+		{#each projects as project, i (i)}
+			<div class="flex w-full justify-center lg:pb-0 pb-16">
+				<Project img={project.img} name={project.name} id={i} description={project.description} tags={project.tags} />
+			</div>
+		{/each}
+	</div>
 </div>
+
+<!-- <div class="pt-40">
+	<h2 class="text-xl text-center">Libraries</h2>
+	<div>
+		<div class="flex w-full justify-between">
+			<div id="seenStack" class="flex w-full justify-center" />
+			<div class="flex w-full justify-center items-center">
+				<button>Previous</button>
+			</div>
+			<div class="flex w-full justify-center">
+
+				<Project
+					img={projects[0].img}
+					name={projects[0].name}
+					description={projects[0].description}
+				/>
+			</div>
+			<div class="flex w-full justify-center items-center">
+				<button>Next</button>
+			</div>
+			<div id="remainingStack" class="relative flex w-full justify-center">
+				{#each projects as project, i (i)}
+					{#if i > 0}
+						<div class="absolute">
+							<Project img={project.img} name={project.name} description={project.description} />
+						</div>
+					{/if}
+				{/each}
+			</div>
+		</div>
+	</div>
+</div>
+<div>
+	<h2 class="text-xl">Full-stack</h2>
+</div>
+<div>
+	<h2 class="text-xl">Backend</h2>
+</div>
+<div>
+	<h2 class="text-xl">Frontend</h2>
+</div> -->
+<!-- {#each projects as project}
+				<Project img={project.img} name={project.name} description={project.description} />
+		{/each} -->
