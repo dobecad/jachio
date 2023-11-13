141403001cec81edddf91724273e0e31eecc523d
jachii <jettymcfiggs@gmail.com>
2023-11-13 03:59:46 -0500

    Formatting

--- app/src/lib/components/Header.svelte
+++ app/src/lib/components/Header.svelte
@@ -1,10 +1,9 @@
 <script>
-
-	import ContactMe from "./ContactMe.svelte";
-import Logo from "./Logo.svelte";
-
+	import ContactMe from './ContactMe.svelte';
+	import Logo from './Logo.svelte';
 </script>
+
 <div class="flex justify-between items-center w-full">
-    <Logo />
-    <ContactMe />
+	<Logo />
+	<ContactMe />
 </div>
