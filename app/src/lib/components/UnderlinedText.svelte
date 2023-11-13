e83b789351316470d53fa266d2885bb4e50202de
jachii <jettymcfiggs@gmail.com>
2023-11-13 03:59:09 -0500

    Underlined text animation

---
+++ app/src/lib/components/UnderlinedText.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let text = "Hover me";
+</script>
+
+<style>
+  .underline {
+    position: relative;
+    color: #9C1F58;
+    text-decoration: none;
+  }
+
+  .underline::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 4px;
+    background-color: #9C1F58; /* Set your underline color */
+    transform-origin: center bottom;
+    transform: scaleX(0);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .underline:hover::after {
+    transform: scaleX(1);
+  }
+  
+  .underline:not(:hover)::after {
+    transform: scaleX(0);
+  }
+</style>
+
+<div class="underline">{text}</div>
