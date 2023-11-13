4ec3974c87148cde626365d750dac0838e2bc6dc
jachii <jettymcfiggs@gmail.com>
2023-11-12 21:40:26 -0500

    Use flex wrap instead of grid so that items stay center

--- app/src/lib/components/Projects.svelte
+++ app/src/lib/components/Projects.svelte
@@ -36,11 +36,15 @@
 </div>
 
 <div class="pt-40">
-	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full gap-16">
+	<div class="flex flex-wrap justify-center w-full gap-16">
 		{#each projects as project, i (i)}
-			<div class="flex w-full justify-center lg:pb-0 pb-16">
-				<Project img={project.img} name={project.name} id={i} description={project.description} tags={project.tags} />
-			</div>
+			<Project
+				img={project.img}
+				name={project.name}
+				id={i}
+				description={project.description}
+				tags={project.tags}
+			/>
 		{/each}
 	</div>
 </div>
