8e22afaa6d7eae45d6b75b73ae191aee48026fa1
jachii <jettymcfiggs@gmail.com>
2023-11-13 04:00:01 -0500

    Formatting and picture update

--- app/src/lib/components/About.svelte
+++ app/src/lib/components/About.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Sectionname from './StyledWord.svelte';
+	import UnderlinedText from './UnderlinedText.svelte';
 </script>
 
 <div id="About" class="flex w-full justify-center text-8xl pb-40">
@@ -8,23 +9,24 @@
 
 <div class="lg:grid lg:grid-cols-2">
 	<div class="flex justify-center">
-		<img src="BlackHole2.png" alt="logo" class="w-80 h-80" />
+		<img src="SpaceHelmet.svg" alt="logo" class="w-80 h-80" />
 	</div>
 	<div class="text-2xl pt-10 lg:pt-0">
 		<p class="flex flex-wrap">
-			I am a software engineer with a passion for simplicity, and I channel that passion into
-			crafting solutions that're here to make your life a whole lot easier.
+			I am a software engineer with a passion for simplicity. I channel that passion into crafting
+			solutions that are here to make your life a whole lot easier. <UnderlinedText />
 		</p>
 		<br />
 		<p class="flex flex-wrap">
 			I have strong interests in distributed systems, full-stack development, API development,
-			container orchestration, and building performant, cost-effective cloud-native applications.
+			container orchestration, infrastructure management, and building performant, cost-effective
+			cloud-native applications.
 		</p>
 		<br />
 		<p class="flex flex-wrap">
 			When I am not trying to make your life easier, I am often either learning some new, bleeding
-			edge technology, trying to get good at a new video game, contributing to open source projects,
-			noodling on the guitar, or catching up on my backlog of engineering books.
+			edge technology, contributing to open source projects, noodling on the guitar, or catching up
+			on my backlog of engineering books.
 		</p>
 	</div>
 </div>
