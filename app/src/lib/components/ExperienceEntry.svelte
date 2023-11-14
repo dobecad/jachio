aea8d607a48e17fcb49de9370412572e17c0665b
jachii <jettymcfiggs@gmail.com>
2023-11-14 02:43:21 -0500

    Use gradient border animation

--- app/src/lib/components/ExperienceEntry.svelte
+++ app/src/lib/components/ExperienceEntry.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
-    export let img: string;
-    export let name: string;
+	export let img: string;
+	export let name: string;
 </script>
 
-<!-- <div class="sm:w-32 h-fit border-slate-200 rounded-xl border-4 w-1/3 overflow-x-auto">
-    <div class="flex justify-center">
-        <img src="{img}" alt="{name}" />
-    </div>
-    <div class="text-2xl text-center pt-4">
-       {name} 
-    </div>
-</div> -->
-
-<div class="w-40 h-40 flex justify-center items-center rounded-xl bg-gradient-to-br from-purple-500 to-blue-500 hover:-translate-y-1 hover:scale-110 ease-in-out duration-500">
-    <div class="w-36 h-36 bg-slate-800 rounded-xl">
-        <div class="w-full h-24 flex justify-center">
-            <img src="{img}" alt="{name}" class="w-24" />
-        </div>
-        <div class="text-2xl text-center">
-           {name} 
-        </div>
-    </div>
+<div
+	class="card w-40 h-40 bg-slate-200 rounded-lg flex justify-center items-center transition hover:duration-300 hover:-translate-y-1 hover:scale-110 ease-in-out"
+>
+	<div class="w-full-minus-5px h-full-minus-5px bg-slate-950 rounded-lg overflow-hidden">
+		<div class="w-full h-28 flex justify-center">
+			<img src={img} alt={name} class="pt-2" />
+		</div>
+		<div class="text-2xl text-center">
+			{name}
+		</div>
+	</div>
 </div>
