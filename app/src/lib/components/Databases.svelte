dd4976f6286a0944df2719691c2353a0d65527f9
jachii <jettymcfiggs@gmail.com>
2023-11-14 03:07:12 -0500

    Databases

---
+++ app/src/lib/components/Databases.svelte
@@ -0,0 +1,24 @@
+
+<script lang="ts">
+	import ExperienceEntry from './ExperienceEntry.svelte';
+	import UnderlinedText from './UnderlinedText.svelte';
+
+	const experience: { img: string; name: string }[] = [
+		{ img: 'logo-mysql.png', name: 'MySQL' },
+		{ img: 'postgresql.svg', name: 'PostgreSQL' },
+		{ img: 'cockroachdb-logo.png', name: 'CockroachDB' },
+		{ img: 'surrealdb-logo.png', name: 'SurrealDB' },
+	];
+</script>
+
+<div id="Databases" class="flex justify-center w-full text-8xl pb-40">
+	<a href="#Databases"><UnderlinedText text={'Databases'} /></a>
+</div>
+
+<div
+	class="flex flex-wrap gap-8 w-full justify-center pb-20"
+>
+	{#each experience as e}
+		<ExperienceEntry img={e.img} name={e.name} />
+	{/each}
+</div>
