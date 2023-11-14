811cc9fa07fe29e65dfdcf0515e15ccd554853c2
jachii <jettymcfiggs@gmail.com>
2023-11-14 03:44:02 -0500

    Add footer

---
+++ app/src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<div class="w-full h-20">
+	<div class="flex flex-wrap w-full justify-center items-center text-lg font-bold text-center">
+		<div class="w-8/12">
+			Designed with Figma, implemented with Sveltekit and TailwindCSS, deployed to Vercel. <br /> SVG
+			elements other than product logos are handmade with Affinity Designer.
+		</div>
+	</div>
+</div>
