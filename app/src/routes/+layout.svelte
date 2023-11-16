e117054505341e58f96a05aeb98070c631a03b6a
jachii <jettymcfiggs@gmail.com>
2023-11-16 00:22:19 -0500

    Move vercel analytics

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-
-	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="w-full text-slate-100">
