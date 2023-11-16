815d69b22022ad38338e4de72a24a265b99e22a1
jachii <jettymcfiggs@gmail.com>
2023-11-15 23:35:27 -0500

    Add vercel analytics

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import "../app.css"
+	import '../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="w-full text-slate-100">
-	<slot></slot>
+	<slot />
 </div>
