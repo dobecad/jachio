a78a5ba5eb35e2a63481a15fc7a75966627a71fd
jachii <jettymcfiggs@gmail.com>
2023-12-04 21:00:33 -0500

    Added Chakra Petch font

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fontsource/chakra-petch';
 </script>
 
-<div class="w-full text-slate-100">
+<div class="w-full text-slate-100 font-chakrapetch">
 	<slot />
 </div>
+
+<style>
+	:global(body) {
+		font-family: 'Chakra Petch', normal;
+	}
+</style>
